b823b7c819f2ab2d39b3c42222851aa6a7198970
= <guilherme_semensato@hotmail.com.br>
2024-05-13 22:46:26 -0300

    feat: static collection realocation

--- UrlShortner/UrlShortner/Program.cs
+++ UrlShortner/UrlShortner/Program.cs
@@ -1,19 +1,11 @@
+using UrlShortner.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.RegisterServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.RegisterMiddlewares();
 
 app.Run();
